2e180c4a4572cec0ee40cdcb3fadf30c8f748144
Pedro <pedrossgg1994@gmail.com>
2017-01-25 21:10:21 +0100

    new ui

--- app/src/main/java/com/pedro/rtmpstreamer/MainActivity.java
+++ app/src/main/java/com/pedro/rtmpstreamer/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
