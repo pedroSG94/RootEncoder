1d095dd4183eeaa6f25888e86752c8378c98bdd3
Pedro <pedrossgg1994@gmail.com>
2017-03-05 17:02:45 +0100

    nothing

--- app/src/main/java/com/pedro/rtmpstreamer/ui/RtspActivity.java
+++ app/src/main/java/com/pedro/rtmpstreamer/ui/RtspActivity.java
@@ -4,6 +4,7 @@ import android.hardware.Camera;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
