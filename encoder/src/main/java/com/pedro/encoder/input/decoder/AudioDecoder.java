a2cee26cdc9a9a5b2fe28835e1be7c39f6b9e66f
Pedro <pedrossgg1994@gmail.com>
2017-06-10 18:36:44 +0200

    test feature

--- encoder/src/main/java/com/pedro/encoder/input/decoder/AudioDecoder.java
+++ encoder/src/main/java/com/pedro/encoder/input/decoder/AudioDecoder.java
@@ -3,10 +3,14 @@ package com.pedro.encoder.input.decoder;
 import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
+import com.pedro.encoder.audio.GetAccData;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by pedro on 9/06/17.
@@ -19,34 +23,133 @@ public class AudioDecoder {
 
   private MediaExtractor audioExtractor;
   private MediaCodec audioDecoder;
+  private final GetAccData getAccData;
+  private MediaCodec.BufferInfo audioInfo = new MediaCodec.BufferInfo();
+  private Thread thread;
   private String filePath;
+  private boolean decoding = false;
+  private int sampleRate;
+  private boolean isStereo;
 
-  public void setFilePath(String filePath) throws FileNotFoundException {
-    File file = new File(filePath);
-    if (file.canRead()) {
-      this.filePath = file.getAbsolutePath();
-    } else {
-      throw new FileNotFoundException("The file can't be read or not exists");
-    }
+  public AudioDecoder(GetAccData getAccData) {
+    this.getAccData = getAccData;
   }
 
-  public void setFilePath(File file) throws FileNotFoundException {
-    if (file.canRead()) {
-      this.filePath = file.getAbsolutePath();
-    } else {
-      throw new FileNotFoundException("The file can't be read or not exists");
-    }
-  }
-
-  public void prepareAudio() {
+  public boolean prepareAudio() {
     try {
       audioExtractor = new MediaExtractor();
       audioExtractor.setDataSource(filePath);
       MediaFormat audioFormat = audioExtractor.getTrackFormat(selectTrack(audioExtractor));
+      sampleRate = audioFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+      isStereo = (audioFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT) == 2);
       audioDecoder = MediaCodec.createDecoderByType(audioFormat.getString(MediaFormat.KEY_MIME));
       audioDecoder.configure(audioFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+      decoding = false;
+      return true;
     } catch (IOException e) {
       Log.e(TAG, "Error preparing audio: ", e);
+      return false;
+    }
+  }
+
+  public void start() {
+    decoding = true;
+    audioDecoder.start();
+    thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        if (Build.VERSION.SDK_INT >= 21) {
+          decodeAudioAPI21();
+        } else {
+          decodeAudio();
+        }
+      }
+    });
+    thread.start();
+  }
+
+  public void stop() {
+    decoding = false;
+    if (thread != null) {
+      thread.interrupt();
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        thread.interrupt();
+      }
+      thread = null;
+    }
+    if (audioExtractor != null) {
+      audioExtractor.release();
+      audioExtractor = null;
+    }
+    if (audioDecoder != null) {
+      audioDecoder.stop();
+      audioDecoder.release();
+      audioDecoder = null;
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  private void decodeAudioAPI21() {
+    while (decoding) {
+      int inBufferIndex = audioDecoder.dequeueInputBuffer(-1);
+      if (inBufferIndex >= 0) {
+        ByteBuffer bb = audioDecoder.getInputBuffer(inBufferIndex);
+        audioExtractor.readSampleData(bb, inBufferIndex);
+        long pts = audioExtractor.getSampleTime();
+        audioDecoder.queueInputBuffer(inBufferIndex, 0, bb.remaining(), pts, 0);
+        audioExtractor.advance();
+      }
+      for (; ; ) {
+        int outBufferIndex = audioDecoder.dequeueOutputBuffer(audioInfo, 0);
+        if (outBufferIndex >= 0) {
+          if ((audioInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+            decoding = false;
+          }
+          if ((audioInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            //This ByteBuffer is ACC
+            ByteBuffer bb = audioDecoder.getOutputBuffer(outBufferIndex);
+            getAccData.getAccData(bb, audioInfo);
+            audioDecoder.releaseOutputBuffer(outBufferIndex, false);
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  private void decodeAudio() {
+    ByteBuffer[] inputBuffers = audioDecoder.getInputBuffers();
+    ByteBuffer[] outputBuffers = audioDecoder.getOutputBuffers();
+    while (decoding) {
+      int inBufferIndex = audioDecoder.dequeueInputBuffer(-1);
+      if (inBufferIndex >= 0) {
+        ByteBuffer bb = inputBuffers[inBufferIndex];
+        bb.clear();
+        audioExtractor.readSampleData(bb, inBufferIndex);
+        long pts = audioExtractor.getSampleTime();
+        audioDecoder.queueInputBuffer(inBufferIndex, 0, bb.remaining(), pts, 0);
+        audioExtractor.advance();
+      }
+
+      for (; ; ) {
+        int outBufferIndex = audioDecoder.dequeueOutputBuffer(audioInfo, 0);
+        if (outBufferIndex >= 0) {
+          if ((audioInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+            decoding = false;
+          }
+          if ((audioInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            //This ByteBuffer is ACC
+            ByteBuffer bb = outputBuffers[outBufferIndex];
+            getAccData.getAccData(bb, audioInfo);
+            audioDecoder.releaseOutputBuffer(outBufferIndex, false);
+          }
+        } else {
+          break;
+        }
+      }
     }
   }
 
@@ -65,5 +168,30 @@ public class AudioDecoder {
       }
     }
     return -1;
+  }
+
+  public void setFilePath(String filePath) throws FileNotFoundException {
+    File file = new File(filePath);
+    if (file.canRead()) {
+      this.filePath = file.getAbsolutePath();
+    } else {
+      throw new FileNotFoundException("The file can't be read or not exists");
+    }
+  }
+
+  public void setFilePath(File file) throws FileNotFoundException {
+    if (file.canRead()) {
+      this.filePath = file.getAbsolutePath();
+    } else {
+      throw new FileNotFoundException("The file can't be read or not exists");
+    }
+  }
+
+  public int getSampleRate() {
+    return sampleRate;
+  }
+
+  public boolean isStereo() {
+    return isStereo;
   }
 }
--- encoder/src/main/java/com/pedro/encoder/input/decoder/VideoDecoder.java
+++ encoder/src/main/java/com/pedro/encoder/input/decoder/VideoDecoder.java
@@ -1,17 +1,17 @@
 package com.pedro.encoder.input.decoder;
 
 import android.media.MediaCodec;
-import android.media.MediaCodecInfo;
-import android.media.MediaCodecList;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
-import com.pedro.encoder.video.FormatVideoEncoder;
+import android.util.Pair;
+import com.pedro.encoder.video.GetH264Data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by pedro on 9/06/17.
@@ -24,36 +24,197 @@ public class VideoDecoder {
 
   private MediaExtractor videoExtractor;
   private MediaCodec videoDecoder;
-
+  private MediaCodec.BufferInfo videoInfo = new MediaCodec.BufferInfo();
+  private Thread thread;
+  private GetH264Data getH264Data;
   private String filePath;
+  private boolean decoding = false;
+  private boolean spsPpsSetted = false;
+  private int bitRate = 1200 * 1024;
 
-  public void setFilePath(String filePath) throws FileNotFoundException {
-    File file = new File(filePath);
-    if (file.canRead()) {
-      this.filePath = file.getAbsolutePath();
-    } else {
-      throw new FileNotFoundException("The file can't be read or not exists");
-    }
+  public VideoDecoder(GetH264Data getH264Data) {
+    this.getH264Data = getH264Data;
   }
 
-  public void setFilePath(File file) throws FileNotFoundException {
-    if (file.canRead()) {
-      this.filePath = file.getAbsolutePath();
-    } else {
-      throw new FileNotFoundException("The file can't be read or not exists");
-    }
-  }
-
-  public void prepareVideo() {
+  public boolean prepareVideo() {
     try {
       videoExtractor = new MediaExtractor();
       videoExtractor.setDataSource(filePath);
       MediaFormat videoFormat = videoExtractor.getTrackFormat(selectTrack(videoExtractor));
+      videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, bitRate);
       videoDecoder = MediaCodec.createDecoderByType(videoFormat.getString(MediaFormat.KEY_MIME));
       videoDecoder.configure(videoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+      decoding = false;
+      return true;
     } catch (IOException e) {
       Log.e(TAG, "Error preparing video: ", e);
+      return false;
     }
+  }
+
+  public void start() {
+    spsPpsSetted = false;
+    decoding = true;
+    videoDecoder.start();
+    thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        if (Build.VERSION.SDK_INT >= 21) {
+          decodeVideoAPI21();
+        } else {
+          decodeVideo();
+        }
+      }
+    });
+    thread.start();
+  }
+
+  public void stop() {
+    decoding = false;
+    spsPpsSetted = false;
+    if (thread != null) {
+      thread.interrupt();
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        thread.interrupt();
+      }
+      thread = null;
+    }
+    if (videoExtractor != null) {
+      videoExtractor.release();
+      videoExtractor = null;
+    }
+    if (videoDecoder != null) {
+      videoDecoder.stop();
+      videoDecoder.release();
+      videoDecoder = null;
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  private void decodeVideoAPI21() {
+    while (decoding) {
+      int inBufferIndex = videoDecoder.dequeueInputBuffer(-1);
+      if (inBufferIndex >= 0) {
+        ByteBuffer bb = videoDecoder.getInputBuffer(inBufferIndex);
+        videoExtractor.readSampleData(bb, inBufferIndex);
+        long pts = videoExtractor.getSampleTime();
+        videoDecoder.queueInputBuffer(inBufferIndex, 0, bb.remaining(), pts, 0);
+        videoExtractor.advance();
+      }
+      for (; ; ) {
+        int outBufferIndex = videoDecoder.dequeueOutputBuffer(videoInfo, 0);
+        if (outBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+          MediaFormat mediaFormat = videoDecoder.getOutputFormat();
+          getH264Data.onSPSandPPS(mediaFormat.getByteBuffer("csd-0"),
+              mediaFormat.getByteBuffer("csd-1"));
+          spsPpsSetted = true;
+        } else if (outBufferIndex >= 0) {
+          if ((videoInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+            decoding = false;
+          }
+          if ((videoInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            if (!spsPpsSetted) {
+              Pair<ByteBuffer, ByteBuffer> buffers =
+                  decodeSpsPpsFromBuffer(videoDecoder.getOutputBuffer(outBufferIndex),
+                      videoInfo.size);
+              if (buffers != null) {
+                getH264Data.onSPSandPPS(buffers.first, buffers.second);
+                spsPpsSetted = true;
+              }
+            }
+          } else {
+            //This ByteBuffer is H264
+            ByteBuffer bb = videoDecoder.getOutputBuffer(outBufferIndex);
+            getH264Data.getH264Data(bb, videoInfo);
+            videoDecoder.releaseOutputBuffer(outBufferIndex, false);
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  private void decodeVideo() {
+    ByteBuffer[] inputBuffers = videoDecoder.getInputBuffers();
+    ByteBuffer[] outputBuffers = videoDecoder.getOutputBuffers();
+    while (decoding) {
+      int inBufferIndex = videoDecoder.dequeueInputBuffer(-1);
+      if (inBufferIndex >= 0) {
+        ByteBuffer bb = inputBuffers[inBufferIndex];
+        bb.clear();
+        videoExtractor.readSampleData(bb, inBufferIndex);
+        long pts = videoExtractor.getSampleTime();
+        videoDecoder.queueInputBuffer(inBufferIndex, 0, bb.remaining(), pts, 0);
+        videoExtractor.advance();
+      }
+
+      for (; ; ) {
+        int outBufferIndex = videoDecoder.dequeueOutputBuffer(videoInfo, 0);
+        if (outBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+          MediaFormat mediaFormat = videoDecoder.getOutputFormat();
+          getH264Data.onSPSandPPS(mediaFormat.getByteBuffer("csd-0"),
+              mediaFormat.getByteBuffer("csd-1"));
+          spsPpsSetted = true;
+        } else if (outBufferIndex >= 0) {
+          if ((videoInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+            decoding = false;
+          }
+          if ((videoInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            if (!spsPpsSetted) {
+              Pair<ByteBuffer, ByteBuffer> buffers =
+                  decodeSpsPpsFromBuffer(outputBuffers[outBufferIndex], videoInfo.size);
+              if (buffers != null) {
+                getH264Data.onSPSandPPS(buffers.first, buffers.second);
+                spsPpsSetted = true;
+              }
+            }
+          } else {
+            //This ByteBuffer is H264
+            ByteBuffer bb = outputBuffers[outBufferIndex];
+            getH264Data.getH264Data(bb, videoInfo);
+            videoDecoder.releaseOutputBuffer(outBufferIndex, false);
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  /**
+   * decode sps and pps if the encoder never call to MediaCodec.INFO_OUTPUT_FORMAT_CHANGED
+   */
+  private Pair<ByteBuffer, ByteBuffer> decodeSpsPpsFromBuffer(ByteBuffer outputBuffer, int length) {
+    byte[] mSPS = null, mPPS = null;
+    byte[] csd = new byte[length];
+    outputBuffer.get(csd, 0, length);
+    int i = 0;
+    int spsIndex = -1;
+    int ppsIndex = -1;
+    while (i < length - 4) {
+      if (csd[i] == 0 && csd[i + 1] == 0 && csd[i + 2] == 0 && csd[i + 3] == 1) {
+        if (spsIndex == -1) {
+          spsIndex = i;
+        } else {
+          ppsIndex = i;
+          break;
+        }
+      }
+      i++;
+    }
+    if (spsIndex != -1 && ppsIndex != -1) {
+      mSPS = new byte[ppsIndex];
+      System.arraycopy(csd, spsIndex, mSPS, 0, ppsIndex);
+      mPPS = new byte[length - ppsIndex];
+      System.arraycopy(csd, ppsIndex, mPPS, 0, length - ppsIndex);
+    }
+    if (mSPS != null && mPPS != null) {
+      return new Pair<>(ByteBuffer.wrap(mSPS), ByteBuffer.wrap(mPPS));
+    }
+    return null;
   }
 
   /**
@@ -71,5 +232,22 @@ public class VideoDecoder {
       }
     }
     return -1;
+  }
+
+  public void setFilePath(String filePath) throws FileNotFoundException {
+    File file = new File(filePath);
+    if (file.canRead()) {
+      this.filePath = file.getAbsolutePath();
+    } else {
+      throw new FileNotFoundException("The file can't be read or not exists");
+    }
+  }
+
+  public void setFilePath(File file) throws FileNotFoundException {
+    if (file.canRead()) {
+      this.filePath = file.getAbsolutePath();
+    } else {
+      throw new FileNotFoundException("The file can't be read or not exists");
+    }
   }
 }
