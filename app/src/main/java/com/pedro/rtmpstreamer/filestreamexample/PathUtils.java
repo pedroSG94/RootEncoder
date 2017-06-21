83c35d2052de0b3ecda4f8b628b2ba4ea79cf752
Pedro <pedrossgg1994@gmail.com>
2017-06-21 23:15:45 +0200

    add file with ui

---
+++ app/src/main/java/com/pedro/rtmpstreamer/filestreamexample/PathUtils.java
@@ -0,0 +1,123 @@
+package com.pedro.rtmpstreamer.filestreamexample;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+
+/**
+ * Created by pedro on 21/06/17.
+ * Get absolute path from onActivityResult
+ * https://stackoverflow.com/questions/33295300/how-to-get-absolute-path-in-android-for-file
+ */
+
+public class PathUtils {
+
+  public static String getPath(final Context context, final Uri uri) {
+
+    // DocumentProvider
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(
+        context, uri)) {
+
+      if (isExternalStorageDocument(uri)) {// ExternalStorageProvider
+        final String docId = DocumentsContract.getDocumentId(uri);
+        final String[] split = docId.split(":");
+        final String type = split[0];
+        String storageDefinition;
+
+        if ("primary".equalsIgnoreCase(type)) {
+
+          return Environment.getExternalStorageDirectory() + "/" + split[1];
+        } else {
+
+          if (Environment.isExternalStorageRemovable()) {
+            storageDefinition = "EXTERNAL_STORAGE";
+          } else {
+            storageDefinition = "SECONDARY_STORAGE";
+          }
+
+          return System.getenv(storageDefinition) + "/" + split[1];
+        }
+      } else if (isDownloadsDocument(uri)) {// DownloadsProvider
+
+        final String id = DocumentsContract.getDocumentId(uri);
+        final Uri contentUri =
+            ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+                Long.valueOf(id));
+
+        return getDataColumn(context, contentUri, null, null);
+      } else if (isMediaDocument(uri)) {// MediaProvider
+        final String docId = DocumentsContract.getDocumentId(uri);
+        final String[] split = docId.split(":");
+        final String type = split[0];
+
+        Uri contentUri = null;
+        if ("image".equals(type)) {
+          contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        } else if ("video".equals(type)) {
+          contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        } else if ("audio".equals(type)) {
+          contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        final String selection = "_id=?";
+        final String[] selectionArgs = new String[] {
+            split[1]
+        };
+
+        return getDataColumn(context, contentUri, selection, selectionArgs);
+      }
+    } else if ("content".equalsIgnoreCase(uri.getScheme())) {// MediaStore (and general)
+
+      // Return the remote address
+      if (isGooglePhotosUri(uri)) return uri.getLastPathSegment();
+
+      return getDataColumn(context, uri, null, null);
+    } else if ("file".equalsIgnoreCase(uri.getScheme())) {// File
+      return uri.getPath();
+    }
+
+    return null;
+  }
+
+  public static String getDataColumn(Context context, Uri uri, String selection,
+      String[] selectionArgs) {
+
+    Cursor cursor = null;
+    final String column = "_data";
+    final String[] projection = {
+        column
+    };
+
+    try {
+      cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+      if (cursor != null && cursor.moveToFirst()) {
+        final int column_index = cursor.getColumnIndexOrThrow(column);
+        return cursor.getString(column_index);
+      }
+    } finally {
+      if (cursor != null) cursor.close();
+    }
+    return null;
+  }
+
+  public static boolean isExternalStorageDocument(Uri uri) {
+    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+  }
+
+  public static boolean isDownloadsDocument(Uri uri) {
+    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+  }
+
+  public static boolean isMediaDocument(Uri uri) {
+    return "com.android.providers.media.documents".equals(uri.getAuthority());
+  }
+
+  public static boolean isGooglePhotosUri(Uri uri) {
+    return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+  }
+}
--- app/src/main/java/com/pedro/rtmpstreamer/filestreamexample/RtspFromFileActivity.java
+++ app/src/main/java/com/pedro/rtmpstreamer/filestreamexample/RtspFromFileActivity.java
@@ -1,13 +1,14 @@
 package com.pedro.rtmpstreamer.filestreamexample;
 
+import android.content.Intent;
 import android.os.Build;
-import android.os.Environment;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.pedro.builder.RtspBuilderFromFile;
 import com.pedro.rtmpstreamer.R;
@@ -19,16 +20,21 @@ public class RtspFromFileActivity extends AppCompatActivity
     implements ConnectCheckerRtsp, View.OnClickListener {
 
   private RtspBuilderFromFile rtspBuilderFromFile;
-  private Button button;
+  private Button button, bSelectFile;
   private EditText etUrl;
+  private TextView tvFile;
+  private String filePath = "";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_rtsp_from_file);
     button = (Button) findViewById(R.id.b_start_stop);
+    bSelectFile = (Button) findViewById(R.id.b_select_file);
     button.setOnClickListener(this);
+    bSelectFile.setOnClickListener(this);
     etUrl = (EditText) findViewById(R.id.et_rtsp_url);
+    tvFile = (TextView) findViewById(R.id.tv_file);
     rtspBuilderFromFile = new RtspBuilderFromFile(Protocol.UDP, this);
   }
 
@@ -85,19 +91,39 @@ public class RtspFromFileActivity extends AppCompatActivity
   }
 
   @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if(requestCode == 5) {
+      filePath = PathUtils.getPath(this, data.getData());
+      Toast.makeText(this, filePath, Toast.LENGTH_SHORT).show();
+      tvFile.setText(filePath);
+    }
+  }
+
+  @Override
   public void onClick(View view) {
-    String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/hola.mp4";
-    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-      if (!rtspBuilderFromFile.isStreaming()) {
-        if (rtspBuilderFromFile.prepareVideo(filePath, 1200 * 1024)
-            && rtspBuilderFromFile.prepareAudio(filePath)) {
-          button.setText(getResources().getString(R.string.stop_button));
-          rtspBuilderFromFile.startStream(etUrl.getText().toString());
+    switch (view.getId()){
+      case R.id.b_start_stop:
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+          if (!rtspBuilderFromFile.isStreaming()) {
+            if (rtspBuilderFromFile.prepareVideo(filePath, 1200 * 1024)
+                && rtspBuilderFromFile.prepareAudio(filePath)) {
+              button.setText(getResources().getString(R.string.stop_button));
+              rtspBuilderFromFile.startStream(etUrl.getText().toString());
+            }
+          } else {
+            button.setText(getResources().getString(R.string.start_button));
+            rtspBuilderFromFile.stopStream();
+          }
         }
-      } else {
-        button.setText(getResources().getString(R.string.start_button));
-        rtspBuilderFromFile.stopStream();
-      }
+        break;
+      case R.id.b_select_file:
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/mp4");
+        startActivityForResult(intent, 5);
+        break;
+      default:
+        break;
     }
   }
 }
