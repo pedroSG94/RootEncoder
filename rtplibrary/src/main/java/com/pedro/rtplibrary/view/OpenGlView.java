6447d693fc5919dbc3028365b205641bf57580c4
Pedro <pedrossgg1994@gmail.com>
2017-09-10 23:16:39 +0200

    fix resolution problem opengl preview

--- rtplibrary/src/main/java/com/pedro/rtplibrary/view/OpenGlView.java
+++ rtplibrary/src/main/java/com/pedro/rtplibrary/view/OpenGlView.java
@@ -104,11 +104,6 @@ public class OpenGlView extends SurfaceView
           if (frameAvailable) {
             frameAvailable = false;
 
-            surfaceManager.makeCurrent();
-            textureManager.updateFrame();
-            textureManager.drawFrame();
-            surfaceManager.swapBuffer();
-
             if (surfaceManagerEncoder != null) {
               surfaceManagerEncoder.makeCurrent();
               textureManager.drawFrame();
@@ -116,6 +111,11 @@ public class OpenGlView extends SurfaceView
               surfaceManagerEncoder.setPresentationTime(ts);
               surfaceManagerEncoder.swapBuffer();
             }
+
+            surfaceManager.makeCurrent();
+            textureManager.updateFrame();
+            textureManager.drawFrame();
+            surfaceManager.swapBuffer();
           } else {
             Log.e(TAG, "No frame received !");
           }
@@ -148,38 +148,5 @@ public class OpenGlView extends SurfaceView
   @Override
   public void surfaceDestroyed(SurfaceHolder holder) {
     stopGlThread();
-  }
-
-  /**
-   * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
-   * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
-   * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
-   *
-   * @param width Relative horizontal size
-   * @param height Relative vertical size
-   */
-  public void setAspectRatio(int width, int height) {
-    if (width < 0 || height < 0) {
-      throw new IllegalArgumentException("Size cannot be negative.");
-    }
-    ratioWidth = width;
-    ratioHeight = height;
-    requestLayout();
-  }
-
-  @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    int width = MeasureSpec.getSize(widthMeasureSpec);
-    int height = MeasureSpec.getSize(heightMeasureSpec);
-    if (0 == ratioWidth || 0 == ratioHeight) {
-      setMeasuredDimension(width, height);
-    } else {
-      if (width < height * ratioWidth / ratioHeight) {
-        setMeasuredDimension(width, width * ratioHeight / ratioWidth);
-      } else {
-        setMeasuredDimension(height * ratioWidth / ratioHeight, height);
-      }
-    }
   }
 }
