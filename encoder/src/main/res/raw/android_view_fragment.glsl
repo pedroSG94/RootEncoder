9b1cd3da33a251cda2740a9b08fc6ec9f7fb4dc8
pedroSG94 <pedrossgg1994@gmail.com>
2018-02-10 13:25:02 +0100

    fix android view and watermark shaders

--- encoder/src/main/res/raw/android_view_fragment.glsl
+++ encoder/src/main/res/raw/android_view_fragment.glsl
@@ -9,5 +9,6 @@ varying vec2 vTextureCoord;
 void main() {
   vec4 color = texture2D(uSampler, vTextureCoord);
   vec4 viewColor = texture2D(uSamplerView, vec2(vTextureCoord.x, 1.0 - vTextureCoord.y));
+  color.rgb *= 1.0 - viewColor.a;
   gl_FragColor = color + viewColor;
 }
--- encoder/src/main/res/raw/watermark_fragment.glsl
+++ encoder/src/main/res/raw/watermark_fragment.glsl
@@ -8,13 +8,12 @@ varying vec2 vTextureCameraCoord;
 varying vec2 vTextureWatermarkCoord;
 
 void main() {
-    vec4 cameraPixel = texture2D(sCamera, vTextureCameraCoord);
-    vec2 coord = vec2(1.0 - vTextureWatermarkCoord.y, 1.0 - vTextureWatermarkCoord.x);
-    vec4 watermarkPixel = texture2D(sWatermark, coord);
-    if (vTextureWatermarkCoord.x < 0.0 || vTextureWatermarkCoord.x > 1.0 ||
-        vTextureWatermarkCoord.y < 0.0 || vTextureWatermarkCoord.y > 1.0 || watermarkPixel.rgb == vec3(0.0, 0.0, 0.0)) {
-      gl_FragColor = cameraPixel;
-    } else {
-      gl_FragColor = (cameraPixel * (1.0 - uAlpha)) + (watermarkPixel * uAlpha);
-    }
+  vec2 coord = vec2(1.0 - vTextureWatermarkCoord.y, 1.0 - vTextureWatermarkCoord.x);
+  vec4 watermarkPixel = texture2D(sWatermark, coord);
+  vec4 cameraPixel = texture2D(sCamera, vTextureCameraCoord);
+  if (watermarkPixel.a < 1.0) {
+    gl_FragColor = cameraPixel;
+  } else {
+    gl_FragColor = (watermarkPixel * uAlpha) + (cameraPixel * (1.0 - uAlpha));
+  }
 }
