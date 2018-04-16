936e58afe92534f4fece30630890f8dfbb718140
pedro <pedrosanchez@droiders.com>
2018-04-16 15:39:01 +0200

    fix image overlay on opaque images

--- encoder/src/main/res/raw/watermark_fragment.glsl
+++ encoder/src/main/res/raw/watermark_fragment.glsl
@@ -8,10 +8,11 @@ varying vec2 vTextureCameraCoord;
 varying vec2 vTextureWatermarkCoord;
 
 void main() {
+  vec4 cameraPixel = texture2D(sCamera, vTextureCameraCoord);
   vec2 coord = vec2(1.0 - vTextureWatermarkCoord.y, 1.0 - vTextureWatermarkCoord.x);
   vec4 watermarkPixel = texture2D(sWatermark, coord);
-  vec4 cameraPixel = texture2D(sCamera, vTextureCameraCoord);
-  if (watermarkPixel.a <= 0.5) {  //0.5 to support antialias on TextStreamObject
+  //0.5 to support antialias on TextStreamObject
+  if (watermarkPixel.a <= 0.5 || coord.x < 0.0 || coord.x > 1.0 || coord.y < 0.0 || coord.y > 1.0) {
     gl_FragColor = cameraPixel;
   } else {
     gl_FragColor = (watermarkPixel * uAlpha) + (cameraPixel * (1.0 - uAlpha));
