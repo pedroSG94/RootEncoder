639a627b75112d561cbfae07a96adbd7d5a0eacd
pedro <pedrosanchez@droiders.com>
2018-08-02 15:14:50 +0200

    refactoring shaders

--- encoder/src/main/res/raw/grey_scale_fragment.glsl
+++ encoder/src/main/res/raw/grey_scale_fragment.glsl
@@ -3,9 +3,10 @@ precision mediump float;
 uniform sampler2D uSampler;
 
 varying vec2 vTextureCoord;
+const vec3 luma = vec3(0.299, 0.587, 0.114);
 
 void main() {
   vec4 pixel = texture2D(uSampler, vTextureCoord);
-  float grey = pixel.r + pixel.g + pixel.b / 3.0;
-  gl_FragColor = vec4(grey, grey, grey, 1.0);
+  float grey = dot(pixel.rgb, luma);
+  gl_FragColor = vec4(grey, grey, grey, pixel.a);
 }
