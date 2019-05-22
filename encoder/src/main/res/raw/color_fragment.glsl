6b3d9215ce703981e593b5b64f575fc1c47abad2
pedro <pedrosanchez@droiders.com>
2019-05-22 08:40:05 +0200

    change color and duotone filters

--- encoder/src/main/res/raw/color_fragment.glsl
+++ encoder/src/main/res/raw/color_fragment.glsl
@@ -4,11 +4,10 @@ uniform sampler2D uSampler;
 uniform vec3 uColor;
 
 varying vec2 vTextureCoord;
+const vec3 luma = vec3(0.299, 0.587, 0.114);
 
 void main() {
   vec4 pixel = texture2D(uSampler, vTextureCoord);
-  float grey = pixel.r + pixel.g + pixel.b / 3.0;
-  vec3 average = vec3(grey, grey, grey);
-  vec4 color = vec4(average.rgb * uColor, 1.0);
-  gl_FragColor = color;
+  float grey = dot(pixel.rgb, luma);
+  gl_FragColor = vec4(grey * uColor, pixel.a);
 }
--- encoder/src/main/res/raw/duotone_fragment.glsl
+++ encoder/src/main/res/raw/duotone_fragment.glsl
@@ -5,9 +5,10 @@ uniform vec3 uColor;
 uniform vec3 uColor2;
 
 varying vec2 vTextureCoord;
+const vec3 luma = vec3(0.299, 0.587, 0.114);
 
 void main() {
   vec4 pixel = texture2D(uSampler, vTextureCoord);
-  float grey = pixel.r + pixel.g + pixel.b / 3.0;
+  float grey = dot(pixel.rgb, luma);
   gl_FragColor = vec4((1.0 - grey) * uColor + grey * uColor2, pixel.a);
 }
