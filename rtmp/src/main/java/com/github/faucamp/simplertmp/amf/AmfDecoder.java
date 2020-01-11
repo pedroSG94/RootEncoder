ea18a328cfbbaf77d6241a87b2a4d46962450da4
pedroSG94 <pedrossgg1994@gmail.com>
2020-01-11 18:54:46 +0100

    fix nullpointer on connection rejected

--- rtmp/src/main/java/com/github/faucamp/simplertmp/amf/AmfDecoder.java
+++ rtmp/src/main/java/com/github/faucamp/simplertmp/amf/AmfDecoder.java
@@ -14,31 +14,35 @@ public class AmfDecoder {
     AmfType amfType = AmfType.valueOf(amfTypeByte);
 
     AmfData amfData;
-    switch (amfType) {
-      case NUMBER:
-        amfData = new AmfNumber();
-        break;
-      case BOOLEAN:
-        amfData = new AmfBoolean();
-        break;
-      case STRING:
-        amfData = new AmfString();
-        break;
-      case OBJECT:
-        amfData = new AmfObject();
-        break;
-      case NULL:
-        return new AmfNull();
-      case UNDEFINED:
-        return new AmfUndefined();
-      case ECMA_MAP:
-        amfData = new AmfMap();
-        break;
-      case STRICT_ARRAY:
-        amfData = new AmfArray();
-        break;
-      default:
-        throw new IOException("Unknown/unimplemented AMF data type: " + amfType);
+    if (amfType != null) {
+      switch (amfType) {
+        case NUMBER:
+          amfData = new AmfNumber();
+          break;
+        case BOOLEAN:
+          amfData = new AmfBoolean();
+          break;
+        case STRING:
+          amfData = new AmfString();
+          break;
+        case OBJECT:
+          amfData = new AmfObject();
+          break;
+        case NULL:
+          return new AmfNull();
+        case UNDEFINED:
+          return new AmfUndefined();
+        case ECMA_MAP:
+          amfData = new AmfMap();
+          break;
+        case STRICT_ARRAY:
+          amfData = new AmfArray();
+          break;
+        default:
+          throw new IOException("Unknown/unimplemented AMF data type: " + amfType);
+      }
+    } else {
+      throw new IOException("Unknown AMF data type");
     }
 
     amfData.readFrom(in);
