28a548e14632ac62718e4bcf03779293371c4689
marinesco <m.zhdanova89@gmail.com>
2020-01-30 20:05:48 +0200

    MicrophoneManager: - Added createMicrophone() with specific audio source - Added createInternalMicrophone() method for internal audio capturing feature in Android Q
    DisplayBase
    - Added prepareInternalAudio() method for capturing internal audio in Android Q

--- encoder/src/main/java/com/pedro/encoder/input/audio/MicrophoneManager.java
+++ encoder/src/main/java/com/pedro/encoder/input/audio/MicrophoneManager.java
@@ -1,8 +1,10 @@
 package com.pedro.encoder.input.audio;
 
 import android.media.AudioFormat;
+import android.media.AudioPlaybackCaptureConfiguration;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
 import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -46,15 +48,24 @@ public class MicrophoneManager {
   }
 
   /**
-   * Create audio record with params
+   * Create audio record with params and default audio source
    */
   public void createMicrophone(int sampleRate, boolean isStereo, boolean echoCanceler,
       boolean noiseSuppressor) {
+    createMicrophone(MediaRecorder.AudioSource.DEFAULT, sampleRate, isStereo, echoCanceler, noiseSuppressor);
+  }
+
+  /**
+   * Create audio record with params and selected audio source
+   * @param audioSource - the recording source. See {@link MediaRecorder.AudioSource} for the recording source definitions.
+   */
+  public void createMicrophone(int audioSource, int sampleRate, boolean isStereo, boolean echoCanceler,
+                               boolean noiseSuppressor) {
     this.sampleRate = sampleRate;
     if (!isStereo) channel = AudioFormat.CHANNEL_IN_MONO;
     audioRecord =
-        new AudioRecord(MediaRecorder.AudioSource.DEFAULT, sampleRate, channel, audioFormat,
-            getPcmBufferSize());
+            new AudioRecord(audioSource, sampleRate, channel, audioFormat,
+                    getPcmBufferSize());
     audioPostProcessEffect = new AudioPostProcessEffect(audioRecord.getAudioSessionId());
     if (echoCanceler) audioPostProcessEffect.enableEchoCanceler();
     if (noiseSuppressor) audioPostProcessEffect.enableNoiseSuppressor();
@@ -62,6 +73,38 @@ public class MicrophoneManager {
     Log.i(TAG, "Microphone created, " + sampleRate + "hz, " + chl);
     created = true;
   }
+
+  /**
+   * Create audio record with params and AudioPlaybackCaptureConfig used for capturing internal audio
+   * Notice that you should granted {@link android.Manifest.permission#RECORD_AUDIO} before calling this!
+   *
+   * @param config - AudioPlaybackCaptureConfiguration received from {@link android.media.projection.MediaProjection}
+   *
+   * @see AudioPlaybackCaptureConfiguration.Builder#Builder(MediaProjection)
+   * @see "https://developer.android.com/guide/topics/media/playback-capture"
+   * @see "https://medium.com/@debuggingisfun/android-10-audio-capture-77dd8e9070f9"
+   */
+  public void createInternalMicrophone(AudioPlaybackCaptureConfiguration config, int sampleRate, boolean isStereo) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      this.sampleRate = sampleRate;
+      if (!isStereo) channel = AudioFormat.CHANNEL_IN_MONO;
+      audioRecord = new AudioRecord.Builder()
+              .setAudioPlaybackCaptureConfig(config)
+              .setAudioFormat(new AudioFormat.Builder()
+                      .setEncoding(audioFormat)
+                      .setSampleRate(sampleRate)
+                      .setChannelMask(channel)
+                      .build())
+              .setBufferSizeInBytes(getPcmBufferSize())
+              .build();
+
+      audioPostProcessEffect = new AudioPostProcessEffect(audioRecord.getAudioSessionId());
+      String chl = (isStereo) ? "Stereo" : "Mono";
+      Log.i(TAG, "Internal microphone created, " + sampleRate + "hz, " + chl);
+      created = true;
+    } else createMicrophone(sampleRate, isStereo, false, false);
+  }
+
 
   /**
    * Start record and get data
