4967db337b21f881a28531c5f1644a92ff5c385b
SouOka <oka.soichiro@donuts.ne.jp>
2020-07-20 15:38:27 +0900

    add: コメントを追記

--- encoder/src/main/java/com/pedro/encoder/utils/CodecUtil.java
+++ encoder/src/main/java/com/pedro/encoder/utils/CodecUtil.java
@@ -291,6 +291,7 @@ public class CodecUtil {
    * For now, none broken codec reported.
    */
   private static boolean isValid(String name) {
+    // 一部エンコーダーで配信ができなかったため、該当エンコーダーのチェックを外す
     return (!name.equals("OMX.qcom.video.encoder.avc") &&
             !name.equals("c2.qti.avc.encoder")) || !Build.MODEL.startsWith("Pixel 3");
   }
