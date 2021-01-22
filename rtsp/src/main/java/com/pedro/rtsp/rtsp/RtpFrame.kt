c2678c87d8f97c9960500d5262c84c65a6c7ac75
pedro <pedro.sanchez@wirelesszt.com>
2021-01-22 13:56:46 +0100

    fix rtps udp

--- rtsp/src/main/java/com/pedro/rtsp/rtsp/RtpFrame.kt
+++ rtsp/src/main/java/com/pedro/rtsp/rtsp/RtpFrame.kt
@@ -1,10 +1,12 @@
 package com.pedro.rtsp.rtsp
 
+import com.pedro.rtsp.utils.RtpConstants
+
 /**
  * Created by pedro on 7/11/18.
  */
 data class RtpFrame(val buffer: ByteArray, val timeStamp: Long, val length: Int,
                     val rtpPort: Int, val rtcpPort: Int, val channelIdentifier: Byte) {
 
-  fun isVideoFrame(): Boolean = channelIdentifier == 2.toByte()
+  fun isVideoFrame(): Boolean = channelIdentifier == (RtpConstants.trackVideo * 2).toByte()
 }
