5d43d6483b6b5f6166e7752ac241894c868331cd
pedro <pedro.sanchez@wirelesszt.com>
2021-04-14 13:02:18 +0200

    version 2.0.1

--- rtmp/src/main/java/net/ossrs/rtmp/ConnectCheckerRtmp.java
+++ rtmp/src/main/java/net/ossrs/rtmp/ConnectCheckerRtmp.java
@@ -1,16 +1,13 @@
 package net.ossrs.rtmp;
 
-import androidx.annotation.NonNull;
-
 /**
  * Created by pedro on 25/01/17.
  */
-
 public interface ConnectCheckerRtmp {
 
   void onConnectionSuccessRtmp();
 
-  void onConnectionFailedRtmp(@NonNull String reason);
+  void onConnectionFailedRtmp(String reason);
 
   void onNewBitrateRtmp(long bitrate);
 
