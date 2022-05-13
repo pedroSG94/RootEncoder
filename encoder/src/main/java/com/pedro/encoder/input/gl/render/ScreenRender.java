77b6dd8192da34fa55eb4045528b094cbc18aef7
yezu-tanaka <76024500+yezu-tanaka@users.noreply.github.com>
2022-05-13 18:40:52 +0900

    fix ratio stream when screen is landscape

--- encoder/src/main/java/com/pedro/encoder/input/gl/render/ScreenRender.java
+++ encoder/src/main/java/com/pedro/encoder/input/gl/render/ScreenRender.java
@@ -139,11 +139,7 @@ public class ScreenRender {
   }
 
   private void draw(int width, int height) {
-    if (height > width) {
-      GLES20.glViewport(0, (int)((float)height * -37.0F / (float)128), width, height);
-    } else {
-      GLES20.glViewport(0, 0, width, height);
-    }
+    GLES20.glViewport(0, (int)(((float)height - 9 * width / (float)16) / (float)-2), width, height);
 
     GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
