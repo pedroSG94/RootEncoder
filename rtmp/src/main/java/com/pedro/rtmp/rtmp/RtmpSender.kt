a1af740248c1ffa12a747aa631c3ed2e65d3e927
rodbailey <rodney.p.bailey@gmail.com>
2023-11-27 15:04:56 +1100

    Added debug statement to RtmpSender.start()

--- rtmp/src/main/java/com/pedro/rtmp/rtmp/RtmpSender.kt
+++ rtmp/src/main/java/com/pedro/rtmp/rtmp/RtmpSender.kt
@@ -114,7 +114,7 @@ class RtmpSender(private val connectCheckerRtmp: ConnectCheckerRtmp,
             socket?.let { socket ->
               size = commandsManager.sendVideoPacket(flvPacket, socket)
               if (isEnableLogs) {
-                Log.i(TAG, "wrote Video packet, size $size")
+                Log.i(TAG, "wrote Video packet, size $size [rpb-test]")
               }
             }
           } else {
@@ -122,7 +122,7 @@ class RtmpSender(private val connectCheckerRtmp: ConnectCheckerRtmp,
             socket?.let { socket ->
               size = commandsManager.sendAudioPacket(flvPacket, socket)
               if (isEnableLogs) {
-                Log.i(TAG, "wrote Audio packet, size $size")
+                Log.i(TAG, "wrote Audio packet, size $size [rpb-test]")
               }
             }
           }
