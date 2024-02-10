594f40896a8e9d4bde8c7c1b8ca8220c0f062b71
pedro <pedrossgg1994@gmail.com>
2024-02-10 03:01:08 +0100

    remove warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,8 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
