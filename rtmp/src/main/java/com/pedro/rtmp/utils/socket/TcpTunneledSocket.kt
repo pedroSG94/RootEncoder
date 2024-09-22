20e7b47a5748d98e2069dd88d296bd255002a5ce
pedroSG94 <pedrossgg1994@gmail.com>
2024-09-22 22:02:07 +0200

    update tcp tunneled implementation

--- rtmp/src/main/java/com/pedro/rtmp/utils/socket/TcpTunneledSocket.kt
+++ rtmp/src/main/java/com/pedro/rtmp/utils/socket/TcpTunneledSocket.kt
@@ -18,6 +18,17 @@ package com.pedro.rtmp.utils.socket
 
 import android.util.Log
 import com.pedro.common.TimeUtils
+import com.pedro.rtmp.utils.readUInt16
+import com.pedro.rtmp.utils.readUInt24
+import com.pedro.rtmp.utils.readUInt32
+import com.pedro.rtmp.utils.readUInt32LittleEndian
+import com.pedro.rtmp.utils.readUntil
+import com.pedro.rtmp.utils.writeUInt16
+import com.pedro.rtmp.utils.writeUInt24
+import com.pedro.rtmp.utils.writeUInt32
+import com.pedro.rtmp.utils.writeUInt32LittleEndian
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.*
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
@@ -36,6 +47,7 @@ class TcpTunneledSocket(private val host: String, private val port: Int, private
     "Content-Type" to "application/x-fcs",
     "User-Agent" to "Shockwave Flash"
   )
+  private val timeout = 5000
   private var connectionId: String = ""
   private var connected = false
   private var index = AtomicLong(0)
@@ -46,78 +58,21 @@ class TcpTunneledSocket(private val host: String, private val port: Int, private
   //send video/audio packets in packs of 10 on each HTTP request.
   private val maxStoredPackets = 10
 
-//  override fun getOutStream(): OutputStream = output
-//
-//  override fun getInputStream(): InputStream {
-//    synchronized(sync) {
-//      val start = TimeUtils.getCurrentTimeMillis()
-//      while (input.available() <= 1 && connected) {
-//        val i = index.addAndGet(1)
-//        val bytes = requestRead("idle/$connectionId/$i", secured)
-//        input = ByteArrayInputStream(bytes, 1, bytes.size)
-//        if (TimeUtils.getCurrentTimeMillis() - start >= timeout) {
-//          throw SocketTimeoutException("couldn't receive a valid packet")
-//        }
-//      }
-//    }
-//    return input
-//  }
-//
-//  override fun flush(isPacket: Boolean) {
-//    synchronized(sync) {
-//      if (isPacket && storedPackets < maxStoredPackets) {
-//        storedPackets++
-//        return
-//      }
-//      if (!connected) return
-//      val i = index.addAndGet(1)
-//      val bytes = output.toByteArray()
-//      output.reset()
-//      requestWrite("send/$connectionId/$i", secured, bytes)
-//      storedPackets = 0
-//    }
-//  }
-//
-//  override fun connect() {
-//    synchronized(sync) {
-//      try {
-//        //optional in few servers
-//        requestWrite("fcs/ident2", secured, byteArrayOf(0x00))
-//      } catch (ignored: IOException) { }
-//      try {
-//        val openResult = requestRead("open/1", secured)
-//        connectionId = String(openResult).trimIndent()
-//        requestWrite("idle/$connectionId/${index.get()}", secured, byteArrayOf(0x00))
-//        connected = true
-//        Log.i(TAG, "Connection success")
-//      } catch (e: IOException) {
-//        Log.e(TAG, "Connection failed: ${e.message}")
-//        connected = false
-//      }
-//    }
-//  }
-//
-//  override fun close() {
-//    Log.i(TAG, "closing tunneled socket...")
-//    connected = false
-//    synchronized(sync) {
-//      Thread {
-//        try {
-//          requestWrite("close/$connectionId", secured, byteArrayOf(0x00))
-//          Log.i(TAG, "Close success")
-//        } catch (e: IOException) {
-//          Log.e(TAG, "Close request failed: ${e.message}")
-//        } finally {
-//          index.set(0)
-//          connectionId = ""
-//        }
-//      }.start()
-//    }
-//  }
-//
-//  override fun isConnected(): Boolean = connected
-//
-//  override fun isReachable(): Boolean = connected
+  private fun getInputStream(requiredBytes: Int): InputStream {
+    if (input.available() >= requiredBytes) return input
+    synchronized(sync) {
+      val start = TimeUtils.getCurrentTimeMillis()
+      while (input.available() <= 1 && connected) {
+        val i = index.addAndGet(1)
+        val bytes = requestRead("idle/$connectionId/$i", secured)
+        input = ByteArrayInputStream(bytes, 1, bytes.size)
+        if (TimeUtils.getCurrentTimeMillis() - start >= timeout) {
+          throw SocketTimeoutException("couldn't receive a valid packet")
+        }
+      }
+    }
+    return input
+  }
 
   @Throws(IOException::class)
   private fun requestWrite(path: String, secured: Boolean, data: ByteArray) {
@@ -162,80 +117,116 @@ class TcpTunneledSocket(private val host: String, private val port: Int, private
       socket.addRequestProperty(key, value)
     }
     socket.doOutput = true
-    socket.connectTimeout = 5000
-    socket.readTimeout = 5000
+    socket.connectTimeout = timeout
+    socket.readTimeout = timeout
     return socket
   }
 
   override suspend fun flush(isPacket: Boolean) {
-    TODO("Not yet implemented")
+    synchronized(sync) {
+      if (isPacket && storedPackets < maxStoredPackets) {
+        storedPackets++
+        return
+      }
+      if (!connected) return
+      val i = index.addAndGet(1)
+      val bytes = output.toByteArray()
+      output.reset()
+      requestWrite("send/$connectionId/$i", secured, bytes)
+      storedPackets = 0
+    }
   }
 
   override suspend fun connect() {
-    TODO("Not yet implemented")
+    synchronized(sync) {
+      try {
+        //optional in few servers
+        requestWrite("fcs/ident2", secured, byteArrayOf(0x00))
+      } catch (ignored: IOException) { }
+      try {
+        val openResult = requestRead("open/1", secured)
+        connectionId = String(openResult).trimIndent()
+        requestWrite("idle/$connectionId/${index.get()}", secured, byteArrayOf(0x00))
+        connected = true
+        Log.i(TAG, "Connection success")
+      } catch (e: IOException) {
+        Log.e(TAG, "Connection failed: ${e.message}")
+        connected = false
+      }
+    }
   }
 
   override suspend fun close() {
-    TODO("Not yet implemented")
+    Log.i(TAG, "closing tunneled socket...")
+    connected = false
+    synchronized(sync) {
+      Thread {
+        try {
+          requestWrite("close/$connectionId", secured, byteArrayOf(0x00))
+          Log.i(TAG, "Close success")
+        } catch (e: IOException) {
+          Log.e(TAG, "Close request failed: ${e.message}")
+        } finally {
+          index.set(0)
+          connectionId = ""
+        }
+      }.start()
+    }
   }
 
-  override fun isConnected(): Boolean {
-    TODO("Not yet implemented")
+  override fun isConnected(): Boolean = connected
+
+  override fun isReachable(): Boolean = connected
+
+  override suspend fun write(b: Int) = withContext(Dispatchers.IO) {
+    output.write(b)
   }
 
-  override fun isReachable(): Boolean {
-    TODO("Not yet implemented")
-  }
-
-  override suspend fun write(b: Int) {
-    TODO("Not yet implemented")
-  }
-
-  override suspend fun write(b: ByteArray) {
-    TODO("Not yet implemented")
+  override suspend fun write(b: ByteArray) = withContext(Dispatchers.IO) {
+    output.write(b)
   }
 
   override suspend fun write(b: ByteArray, offset: Int, size: Int) {
-    TODO("Not yet implemented")
+    output.write(b, offset, size)
   }
 
   override suspend fun writeUInt16(b: Int) {
-    TODO("Not yet implemented")
+    output.writeUInt16(b)
   }
 
   override suspend fun writeUInt24(b: Int) {
-    TODO("Not yet implemented")
+    output.writeUInt24(b)
   }
 
   override suspend fun writeUInt32(b: Int) {
-    TODO("Not yet implemented")
+    output.writeUInt32(b)
   }
 
   override suspend fun writeUInt32LittleEndian(b: Int) {
-    TODO("Not yet implemented")
+    output.writeUInt32LittleEndian(b)
   }
 
-  override suspend fun read(): Int {
-    TODO("Not yet implemented")
+  override suspend fun read(): Int = withContext(Dispatchers.IO) {
+    getInputStream(1).read()
   }
 
   override suspend fun readUInt16(): Int {
-    TODO("Not yet implemented")
+    return getInputStream(1).readUInt16()
   }
 
   override suspend fun readUInt24(): Int {
-    TODO("Not yet implemented")
+    return getInputStream(1).readUInt24()
   }
 
   override suspend fun readUInt32(): Int {
-    TODO("Not yet implemented")
+    return getInputStream(1).readUInt32()
   }
 
   override suspend fun readUInt32LittleEndian(): Int {
-    TODO("Not yet implemented")
+    return getInputStream(1).readUInt32LittleEndian()
   }
 
   override suspend fun readUntil(b: ByteArray) {
-    TODO("Not yet implemented")
+    return getInputStream(b.size).readUntil(b)
   }
 }
