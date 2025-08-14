27cfd99f7a7505789b1b8803b7739f315c4bbf50
pedroSG94 <pedrossgg1994@gmail.com>
2025-08-14 23:51:20 +0200

    optmize AudioUtils

--- encoder/src/main/java/com/pedro/encoder/input/audio/AudioUtils.kt
+++ encoder/src/main/java/com/pedro/encoder/input/audio/AudioUtils.kt
@@ -9,20 +9,14 @@ class AudioUtils {
     ) {
         if (buffer.size != buffer2.size) return
         for (i in buffer.indices step 2) {
-            val sample1 = ((buffer[i + 1].toInt() shl 8) or (buffer[i].toInt() and 0xFF))
-            val sample2 = ((buffer2[i + 1].toInt() shl 8) or (buffer2[i].toInt() and 0xFF))
+            val sample1 = ((buffer[i + 1].toInt() shl 8) or (buffer[i].toInt() and 0xFF)).toShort().toInt()
+            val sample2 = ((buffer2[i + 1].toInt() shl 8) or (buffer2[i].toInt() and 0xFF)).toShort().toInt()
 
-            val signedSample1 = if (sample1 > 32767) sample1 - 65536 else sample1
-            val signedSample2 = if (sample2 > 32767) sample2 - 65536 else sample2
-
-            val adjustedSample1 = (signedSample1 * volume).toInt()
-            val adjustedSample2 = (signedSample2 * volume2).toInt()
-            var mixedSample = adjustedSample1 + adjustedSample2
-
-            mixedSample = mixedSample.coerceIn(-32768, 32767)
-            val unsignedSample = if (mixedSample < 0) mixedSample + 65536 else mixedSample
-            dst[i] = (unsignedSample and 0xFF).toByte()
-            dst[i + 1] = ((unsignedSample shr 8) and 0xFF).toByte()
+            val adjustedSample1 = (sample1 * volume).toInt()
+            val adjustedSample2 = (sample2 * volume2).toInt()
+            val mixedSample = (adjustedSample1 + adjustedSample2).coerceIn(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt()) and 0xFFFF
+            dst[i] = (mixedSample and 0xFF).toByte()
+            dst[i + 1] = ((mixedSample shr 8) and 0xFF).toByte()
         }
     }
 
@@ -30,10 +24,10 @@ class AudioUtils {
         if (volume == 1f) return
 
         for (i in buffer.indices step 2) {
-            val sample = ((buffer[i + 1].toInt() shl 8) or (buffer[i].toInt() and 0xFF))
-            val adjustedSample = (sample * volume).toInt()
-            buffer[i] = adjustedSample.toByte()
-            buffer[i + 1] = (adjustedSample shr 8).toByte()
+            val sample = ((buffer[i + 1].toInt() shl 8) or (buffer[i].toInt() and 0xFF)).toShort().toInt()
+            val adjustedSample = (sample * volume).toInt().coerceIn(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt()) and 0xFFFF
+            buffer[i] = (adjustedSample and 0xFF).toByte()
+            buffer[i + 1] = ((adjustedSample shr 8) and 0xFF).toByte()
         }
     }
 
