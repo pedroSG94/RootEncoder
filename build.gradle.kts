1770668a205001c2dfd7ccd6b1be3eb15f15671e
pedroSG94 <pedrossgg1994@gmail.com>
2025-08-25 09:45:46 +0200

    change license url

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
           licenses {
             license {
               name = "Apache-2.0"
-              url = "https://opensource.org/license/apache-2-0"
+              url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
               distribution = "manual"
             }
           }
