33026d3fdad1e7dcea6670fbc02ed297ec83f9bd
paul <paul@mayflower.work>
2026-01-22 17:10:54 +0100

    Returned release plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,26 @@
 allprojects {
-  group = "com.github.paul-kowlman"
+    group = "com.github.pedroSG94"
+    version = "2.6.7"
 }
 
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.android.library) apply false
-  alias(libs.plugins.jetbrains.kotlin) apply false
-  alias(libs.plugins.jetbrains.dokka) apply true
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin) apply false
+    alias(libs.plugins.jetbrains.dokka) apply true
 }
 
 dependencies {
-  dokka(project(":common"))
-  dokka(project(":encoder"))
-  dokka(project(":extra-sources"))
-  dokka(project(":library"))
-  dokka(project(":rtmp"))
-  dokka(project(":rtsp"))
-  dokka(project(":srt"))
-  dokka(project(":udp"))
+    dokka(project(":common"))
+    dokka(project(":encoder"))
+    dokka(project(":extra-sources"))
+    dokka(project(":library"))
+    dokka(project(":rtmp"))
+    dokka(project(":rtsp"))
+    dokka(project(":srt"))
+    dokka(project(":udp"))
 }
 
 tasks.named<org.jetbrains.dokka.gradle.tasks.DokkaGeneratePublicationTask>("dokkaGeneratePublicationHtml") {
-  outputDirectory.set(layout.projectDirectory.dir("docs"))
+    outputDirectory.set(layout.projectDirectory.dir("docs"))
 }
