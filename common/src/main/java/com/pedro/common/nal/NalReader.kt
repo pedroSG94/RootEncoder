81a750abc3b1456d9a3ce1e1dab218b5e69d6c9d
pedroSG94 <pedrossgg1994@gmail.com>
2026-06-09 02:59:33 +0200

    improve discard nals performance

--- common/src/main/java/com/pedro/common/nal/NalReader.kt
+++ common/src/main/java/com/pedro/common/nal/NalReader.kt
@@ -39,7 +39,8 @@ object NalReader {
           val duplicate = buffer.duplicate()
           duplicate.position(payloadStart - offset)
           duplicate.limit(previousPayloadEnd - offset)
-          units.add(duplicate.slice())
+          val nal = duplicate.slice()
+          if (shouldKeepNal(nal, codec, shouldDiscardVideoInfo)) units.add(nal)
         }
         payloadStart = i + 3
         i += 3
@@ -53,22 +54,21 @@ object NalReader {
       duplicate.limit(limit - offset)
       units.add(duplicate.slice())
     }
-    units.removeAll { shouldDiscardNal(it, codec, shouldDiscardVideoInfo) }
     if (units.isEmpty()) units.add(buffer)
     return units
   }
 
-  private fun shouldDiscardNal(nal: ByteBuffer, codec: VideoCodec, shouldDiscardVideoInfo: Boolean): Boolean {
+  private fun shouldKeepNal(nal: ByteBuffer, codec: VideoCodec, shouldDiscardVideoInfo: Boolean): Boolean {
     return when (codec) {
       VideoCodec.H264 -> {
         val type = (nal.get(0) and 0x1F).toInt()
-        type == H264_SEI || type == H264_AUD ||
-            (shouldDiscardVideoInfo && (type == H264_SPS || type == H264_PPS))
+        !(type == H264_SEI || type == H264_AUD ||
+            (shouldDiscardVideoInfo && (type == H264_SPS || type == H264_PPS)))
       }
       VideoCodec.H265 -> {
         val type = nal.get(0).toInt().shr(1) and 0x3F
-        type == H265_PRE_SEI || type == H265_SU_SEI || type == H265_AUD ||
-            (shouldDiscardVideoInfo && (type == H265_SPS || type == H265_PPS || type == H265_VPS))
+        !(type == H265_PRE_SEI || type == H265_SU_SEI || type == H265_AUD ||
+            (shouldDiscardVideoInfo && (type == H265_SPS || type == H265_PPS || type == H265_VPS)))
       }
       else -> false
     }
