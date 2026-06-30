389f9337f4811f80a0d378451982e6b7c341ce5c
pedroSG94 <pedrossgg1994@gmail.com>
2026-07-01 00:53:58 +0200

    add spsh265 parser test

--- rtmp/src/test/java/com/pedro/rtmp/flv/video/VideoConfigTest.kt
+++ rtmp/src/test/java/com/pedro/rtmp/flv/video/VideoConfigTest.kt
@@ -16,10 +16,12 @@
 
 package com.pedro.rtmp.flv.video
 
+import com.pedro.rtmp.flv.video.config.SPSH265Parser
 import com.pedro.rtmp.flv.video.config.VideoSpecificConfigAV1
 import com.pedro.rtmp.flv.video.config.VideoSpecificConfigAVC
 import com.pedro.rtmp.flv.video.config.VideoSpecificConfigHEVC
 import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class VideoConfigTest {
@@ -69,5 +71,25 @@ class VideoConfigTest {
     val data = ByteArray(config.size)
     config.write(data, 0)
     assertArrayEquals(expectedConfig, data)
+  }
+
+  @Test
+  fun `GIVEN a real H265 sps WHEN parse THEN decode the profile_tier_level fields`() {
+    val sps = byteArrayOf(
+      66, 1, 1, 1, 96, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, -103, -96, 15, 8, 2, -127,
+      104, -76, -82, -55, 46, -26, -96, -64, -64, -64, 16
+    )
+    val parser = SPSH265Parser()
+    parser.parse(sps)
+
+    assertEquals(0, parser.generalProfileSpace)
+    assertEquals(0, parser.generalTierFlag)
+    assertEquals(1, parser.generalProfileIdc)
+    assertEquals(0x60000000, parser.generalProfileCompatibilityFlags)
+    assertEquals(0L, parser.generalConstraintIndicatorFlags)
+    assertEquals(153, parser.generalLevelIdc)
+    assertEquals(1, parser.chromaFormat)
+    assertEquals(0, parser.bitDepthLumaMinus8)
+    assertEquals(0, parser.bitDepthChromaMinus8)
   }
 }
